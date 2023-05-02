355eb022d3666b5f8abc6d0b8c8fb6250b0945de
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-05-02 10:14:34 +0900

    add dimens

--- app/src/main/java/com/example/woof/MainActivity.kt
+++ app/src/main/java/com/example/woof/MainActivity.kt
@@ -52,6 +52,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -113,6 +114,17 @@ fun DogItem(dog: Dog, modifier: Modifier = Modifier) {
             Spacer(modifier = Modifier.weight(1f))
             DogItemButton(expanded = expanded,
                 onClick = { expanded != expanded })
+            if (expanded) {
+                DogHobby(
+                    dogHobby = dog.hobbies,
+                    modifier = Modifier.padding(
+                        start = dimensionResource(id = R.dimen.padding_medium),
+                        end = dimensionResource(id = R.dimen.padding_medium),
+                        top = dimensionResource(id = R.dimen.padding_small),
+                        bottom = dimensionResource(id = R.dimen.padding_medium)
+                    )
+                )
+            }
         }
     }
 }
@@ -193,8 +205,10 @@ fun DogHobby(@StringRes dogHobby: Int, modifier: Modifier = Modifier) {
                 end = 16.dp
             )
     ) {
-        Text(text = stringResource(id = R.string.about),
-        style = MaterialTheme.typography.h3)
+        Text(
+            text = stringResource(id = R.string.about),
+            style = MaterialTheme.typography.h3
+        )
 
     }
 }
